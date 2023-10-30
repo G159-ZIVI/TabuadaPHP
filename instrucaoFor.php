3f540229233f0e42083c6cf5ce25434373c2a1fc
G159-ZIVI <114368786+G159-ZIVI@users.noreply.github.com>
2023-10-30 12:05:22 -0300

    tabuada Instrução For
    repositório aula Faculdade

---
+++ instrucaoFor.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instrução FOR Aninhamento  </title>
+    <style> .cor1{
+                    color: #f00;
+               }
+               .cor2{
+                    color: #0f0;
+               }
+               div{
+                    font-size:15px;
+                    width:200px;
+                    border: 1px solid #000;
+                    margin:5px;
+                    padding:0px 10px 10px;
+                    font-family:tahoma;
+                    float:left;
+                    }     
+               h2{
+                    text-align:center;
+                    font-size:18px;
+               }</style>
+</head>
+<body>
+<?php for ($i=1;$i<=10;$i++) { ?>
+                    <div>
+                         <h2>Tabuada do <?php echo $i?></h2>
+                         <?php for ($j=1; $j<10; $j++){ ?>
+                              <span class="cor1"><?php echo $i; ?></span> x <span class="cor2"><?php echo $j; ?></span> = <?php echo $i * $j; ?> <br/>
+                         <?php } ?>
+                    </div>
+               <?php } ?>
+</body>
+</html>
